6ea3311f620808a4b57e26ee80cd394f664077f7
AdrianMendezVargas <velociraptor088@gmail.com>
2020-04-17 16:50:10 -0400

    Agregando Opciones de exportacion de personas, agregando SweetAlert.

---
+++ Client/Helpers/IJSExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyApp.Client.Helpers {
+    public static class IJSExtensions {
+
+        public static ValueTask<object> GuardarComo(this IJSRuntime js, string nombreArchivo, byte[] archivo) {
+
+            return js.InvokeAsync<object>("saveAsFile" , nombreArchivo , Convert.ToBase64String(archivo));
+        }
+
+        public static ValueTask<object> MostrarMensaje(this IJSRuntime js, string mensaje) {
+
+            return js.InvokeAsync<object>("Swal.fire" , mensaje);
+
+        }
+
+        public static ValueTask<object> MostrarMensaje(this IJSRuntime js ,  string titulo, string mensaje, TipoMensaje tipoMensaje) {
+
+            return js.InvokeAsync<object>("Swal.fire" , titulo, mensaje, tipoMensaje.ToString());
+
+        }
+        
+        public enum TipoMensaje {
+            question, warning, error, success, info
+        }
+
+    }
+}
